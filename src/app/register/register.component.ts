16a08a7ba8a18a9ff93a5aa4acc324fb4756f205
Vivek-341 <vivekvardhannada@gmail.com>
2025-03-22 09:04:44 +0530

    22-03-2025 Commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  
 }
